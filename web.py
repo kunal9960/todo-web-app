50d24e6845b80c7cfc4fd64ce0f0235472b6c720
Kunal <kunald9960@gmail.com>
2024-03-11 17:19:34 +0530

    Made to revert

--- web.py
+++ web.py
@@ -1,28 +1,34 @@
 import streamlit as st
 import functions
 
-todos = functions.get_todos()
+# Initialize session state
+if 'todos' not in st.session_state:
+    st.session_state.todos = functions.get_todos()
 
 
+# Define Streamlit app content
 def add_todo():
     todo = st.session_state["new_todo"] + "\n"
-    todos.append(todo)
-    functions.write_todos(todos)
+    st.session_state.todos.append(todo)
+    functions.write_todos(st.session_state.todos)
+
+def main():
+    st.title("My Todo App")
+    st.subheader("Welcome to my todo app!")
+    st.write("This app helps to increase your <b>productivity</b>.", unsafe_allow_html=True)
+
+    for index, todo in enumerate(st.session_state.todos):
+        checkbox = st.checkbox(todo, key=todo)
+        if checkbox:
+            st.session_state.todos.pop(index)
+            functions.write_todos(st.session_state.todos)
+            del st.session_state[todo]
+            st.rerun()
+
+    st.text_input(label="", placeholder="Add new todo...",
+                  on_change=add_todo, key='new_todo')
 
 
-st.title("My Todo App")
-st.subheader("Welcome to my todo app!")
-st.write("This app helps to increase your <b>productivity</b>.",
-         unsafe_allow_html=True)
-
-
-for index, todo in enumerate(todos):
-    checkbox = st.checkbox(todo, key=todo)
-    if checkbox:
-        todos.pop(index)
-        functions.write_todos(todos)
-        del st.session_state[todo]
-        st.rerun()
-
-st.text_input(label="", placeholder="Add new todo...",
-              on_change=add_todo, key='new_todo')
+# Run Streamlit app
+if __name__ == "__main__":
+    main()
